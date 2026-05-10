8926f34d374dd4f7debc642f327d349fc46e4c64
ishwarpharma <mail@ishwarpharma.com>
2026-05-10 12:55:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open('collectpro-v1').then(cache =>
-      cache.match(event.request).then(response =>
-        response ||
-        fetch(event.request).then(fetchRes => {
-          cache.put(event.request, fetchRes.clone());
-          return fetchRes;
-        })
-      )
-    )
-  );
-});
+self.addEventListener('fetch',()=>{});
